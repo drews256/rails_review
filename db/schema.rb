2cec99840cfee52ded8f1d79f971416d3ca414ee
drews256 <andrewstuntz@hotmail.com>
2016-09-29 20:26:20 -0600

    rebuilding schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160929012041) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
     t.integer  "user_id"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20160929012041) do
     t.text     "last_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
